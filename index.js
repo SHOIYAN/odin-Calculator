b4b553a3c964c454dae5bacaeea3564296aaddf2
SHOIYAN <shoiyan9@gmail.com>
2025-09-29 20:57:48 +0300

    feat: add operator handling

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function multiply(a, b) {
 function divide(a, b) {
   return b === 0 ? "Brah??" : parseFloat((a / b).toFixed(4));
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}

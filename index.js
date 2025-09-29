71ec8c695c2d079c81b6fcf5bf60d7ba3432ec9b
SHOIYAN <shoiyan9@gmail.com>
2025-09-29 20:53:18 +0300

    feat: implement basic math operation functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function add(a, b) {
+  return parseFloat((a + b).toFixed(4));
+}
+function subtract(a, b) {
+  return parseFloat((a - b).toFixed(4));
+}
+function multiply(a, b) {
+  return parseFloat((a * b).toFixed(4));
+}
+function divide(a, b) {
+  return b === 0 ? "Brah??" : parseFloat((a / b).toFixed(4));
+}
